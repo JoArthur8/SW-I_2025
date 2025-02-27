46ac57b8cc3de9cb0642dca009b740a27c5e651d
João Arthur <159496886+JoArthur8@users.noreply.github.com>
2025-02-27 08:58:32 -0300

    Add files via upload

---
+++ aula03/exercicio.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_POST['cxnome'];
+    $mensagem = $_POST['cxmensagem'];
+
+    echo "<p>Olá $nome, sua mensagem é: $mensagem</p>"
+    
+?>
---
+++ aula03/processa01.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_REQUEST['cxnome'];
+    $idade = $_REQUEST['cxidade'];
+
+    echo "<p>O seu nome é $nome</p>";
+    echo "<p>A sua idade é $idade</p>";
+?>
